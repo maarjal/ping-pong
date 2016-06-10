9c245987e5f7d267909f4413fffd4d99c0d26f75
Maarja Laineste <maarjalaineste@Maarjas-MacBook-Pro.local>
2016-06-10 08:56:49 -0700

    add first spec for returning a range of numbers from 1 up to inserted number

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+var numArray = [];
+var result;
+var pingPong = function(number) {
+  for (var i = 1; i <= number; i++) {
+    numArray.push(i);
+  }
+  return result;
+};
+
+
+
+$(document).ready(function() {
+  $("form#ping-pong").submit(function(event) {
+    event.preventDefault();
+    var number = parseInt($("input#number").val());
+    $("form#ping-pong").each(function() {
+      this.reset();
+    });
+    var result = pingPong(number);
+
+    var counter = 0;
+      numArray.forEach(function(list) {
+          $(".list").append("<li>" + numArray[counter] + "</li>");
+          counter++;
+      });
+      $("#result").show();
+    });
+  });
